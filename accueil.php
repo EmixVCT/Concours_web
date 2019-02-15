e5fc866346766b1fbf6b17ee8f1a007c72a4d89c
DocDada <32818113+DocDada@users.noreply.github.com>
2019-02-15 15:50:10 +0100

    Add files via upload

--- accueil.php
+++ accueil.php
@@ -8,6 +8,66 @@ if (!estConnecte() OR $_SESSION['role'] != "chercheur") { #Si on arrive sur cett
     exit;
 }
 ?>
+<script src="scripts/afficher_chercheurs.js" type="text/javascript"></script>
+
+
+<div class="row mbr-justify-content-center">
+	<div class="col-lg-6 mbr-col-md-10">
+        <div class="wrap">
+		
+			<!-- Formulaire recherche -->
+			<div class='row'>
+				<div class='col-xs-1 col-sm-11 col-md-8 col-lg-6'>
+					<h4>Recherche de ressources :</h4>
+				</div>
+			</div>
+			<form name='formulaire_ressource'>	
+				<div class='row'>	
+					<div class='col-xs-1 col-sm-11 col-md-8 col-lg-6'>
+						<div class="input-group mb-1">
+							<div class="input-group-prepend">
+								<span class="input-group-text" id="nom_input">Nom</span>
+							</div>
+							<input type="text" class="form-control" aria-describedby="nom_input" name='nom'>
+						</div>
+					</div>
+				
+					<button id='recherche_ressource' type="button" name='recherche_ressource' value='Rechercher' class='btn btn-outline-secondary'>Rechercher</button>
+				</div><br/>
+			</form>
+			
+			<div id='ressources'></div>
+		</div>
+	</div>
+	
+	
+	<div class="col-lg-6 mbr-col-md-10">
+        <div class="wrap">
+			<div class='row'>
+				<div class='col-xs-1 col-sm-11 col-md-8 col-lg-6'>
+					<h4>Historique :</h4>
+				</div>
+			</div>
+			<form name='formulaire_historique'>	
+				<div class='row'>	
+					<div class='col-xs-1 col-sm-11 col-md-8 col-lg-6'>
+						<div class="input-group mb-1">
+							<div class="input-group-prepend">
+								<span class="input-group-text" id="nom_input">Nom</span>
+							</div>
+							<input type="text" class="form-control" aria-describedby="nom_input" name='nom'>
+						</div>
+					</div>
+				
+				<button id='recherche_historique' type="button" name='recherche_historique' value='Rechercher' class='btn btn-outline-secondary'>Rechercher</button>
+				</div><br/>
+			</form>
+			
+			<div id='historique'></div>
+		</div>
+	</div>
+</div>
+	
 
 <form class="form-group" action="logout.php" method="post">
 	<button type="submit" class="btn btn-danger" style="margin:20px;">Se déconnecter</button>
