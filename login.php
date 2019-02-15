e16128ecfa57030da71b8bbca4d3f35beba98947
zlezero <tom.new2010@gmail.com>
2019-02-15 12:04:00 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once('config.php');
 
 if ( (isset($_POST['id']) && isset($_POST['pwd'])) AND !(empty($_POST['id']) && empty($_POST['pwd'])) ) { #On vérifie la validité du formulaire
@@ -6,36 +7,41 @@ if ( (isset($_POST['id']) && isset($_POST['pwd'])) AND !(empty($_POST['id']) && 
 		$login = htmlspecialchars($_POST["id"]);
 		$mdp = hash("sha512",htmlspecialchars($_POST["pwd"]));	
 		$req = "SELECT * FROM utilisateur WHERE nom like \"$login\" and mdp like \"$mdp\"";
-		
-		if ($res = mysqli_query($connexion,$req)){ //test si la commande est bien exec
-			$rowcount=mysqli_num_rows($res);
-			
-			if ($rowcount == 1) { //test si on a un resultat
-				$lig = mysqli_fetch_assoc($res);
-				
-				$_SESSION['id'] = $lig['nom'];
 
-				if($lig['rang'] == 1){ //Si c'est un chercheur
+		$res = $connexion->query($req);
+
+		while ($donnees = $res->fetch())
+		{
+			print_r($donnees);
+
+			if ($donnees['nom'] == $_POST['id'] AND $donnees["mdp"] == hash("sha512", $_POST["pwd"])) {
+				
+				$_SESSION['id'] = $donnees["nom"];
+
+				if($donnees['rang'] == 1){ //Si c'est un chercheur
 					$_SESSION['role'] = "chercheur";
 					header('Location: accueil.php');
 					exit;
-				}else{ //Sinon c'est un admin
+				}
+				else //Sinon c'est un admin
+				{ 
 					$_SESSION['role'] = "admin";
 					header('Location: admin.php');
 					exit;
 				}
 
-			}else{ //pas de mdp et login correct trouver (redirection)
-				header('Location: connect.php?erreur='.sha1("C'est une erreur !"));
-				exit;
 			}
-			
-		}else{ 
-			echo "Impossible d\'accéder à la table UTILISATEUR !";
 		}
+
+		$res->closeCursor();
+	 	//Pas de mdp et login correct trouvés (redirection)
+		header('Location: connect.php?erreur='.sha1("C'est une erreur !"));
+		exit;
+
 }
 
 else { #Si l'envoi du formulaire est incorrect ou que l'on accède à la page d'une autre façon
+
     if (estConnecte()) { #Si on est déjà connecté lorsque on accède à la page
         redirigerBonnePage();
     }
