3d92a142bce00f30268448a1b7d7b5a5e48a3bb4
zlezero <tom.new2010@gmail.com>
2019-02-15 13:35:39 +0100

    Add files via upload

--- scripts/get_ressource.php
+++ scripts/get_ressource.php
@@ -1,36 +1,35 @@
 <?php
 
 require_once("../config.php");
-$requete="SELECT * FROM ressources";
+
 
 if (isset($_POST['nom']) and !empty($_POST['nom'])){
-	$requete .= " where nom like '%".$_POST['nom']."%' ";
+	$req = $connexion->prepare('SELECT nom FROM ressources WHERE nom LIKE %?%');
+	$req->execute(array($_POST['nom']));
 }
-$requete .= ";";
+else {
+	$req="SELECT nom FROM ressources";
+	$req = $connexion->query($req);
+}
 
-//requete passee dans la commande  mysql_query
-$resultat = mysqli_query($connexion,$requete);
-
-//affichage du resultat utilisation de la commande mysql_fetch_row si il y a au moins 1 ligne
-if (mysqli_num_rows($resultat) != 0){ ?>
+if ($req->rowCount() != 0)
+{ ?>
 	<div class="table-responsive table-wrapper-scroll-y table-sm table-condenced ">
 		<table id='tab_ressources' border=1 align='center' class="table table-striped">
 			<thead align="center"><tr>
-				<th align="center" >NOM</th>
-				<th></th>
+				<th align="center">NOM</th>
+				<th></th><th></th>
 			</tr></thead>
 		<tbody>
 		<?php
-		while ($ligne=mysqli_fetch_row($resultat)) {	
+
+
+		while ($donnees = $req->fetch()) {	
 			echo "<tr>";
-			foreach($ligne as $k => $val){	
-				if ($k != 0){
-					echo '<td align="center">'. $val.'</td>';
-				}
-			}
+			echo '<td align="center">'. $donnees['nom'].'</td>';
 			?>
-			<td align="center"> <input type='button' class="btn btn-outline-danger" name='supp' value='supprimer' onclick='supprimerLig("<?php echo $ligne[0]; ?>","id_rsc","info")'/></td>
-			<!--<td align="center"> <input type='button' class="btn btn-outline-primary" name='modif' value='modifier' onclick='modifierEtu("<?php //echo $ligne[0]; ?>")'/></td>-->
+			<td align="center"> <input type='button' class="btn btn-outline-danger" name='supp' value='supprimer' onclick='supprimerLig("<?php echo $ligne[0]; ?>","id","info")'/></td>
+			<td align="center"> <input type='button' class="btn btn-outline-primary" name='modif' value='modifier' onclick='modifierEtu("<?php echo $ligne[0]; ?>")'/></td>
 			
 			</tr>
 			<?php
@@ -43,6 +42,6 @@ else{
 	echo "Aucun résultat trouvé !<br/><br/>";
 }
 
-
+$req->closeCursor();
 
 ?>
