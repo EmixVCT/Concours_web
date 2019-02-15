0e805b3a56e949b4c320b9fb69a7de7617073884
EmixVCT <39571952+EmixVCT@users.noreply.github.com>
2019-02-15 10:44:20 +0100

    Add files via upload

--- admin.php
+++ admin.php
@@ -1,7 +1,7 @@
 <?php
 //On inclut les fichiers nécessaires
 require_once("config.php");
-require_once($fichiersInclude.'head.php');
+require_once($fichiersInclude.'header.html');
 
 if (!estConnecte() OR $_SESSION['role'] != "admin") { #Si on arrive sur cette page alors que l'on est pas connecté / ou que l'on n'est pas un administrateur
     header('Location: index.php'); #On redirige vers la page de connexion
@@ -15,5 +15,5 @@ if (!estConnecte() OR $_SESSION['role'] != "admin") { #Si on arrive sur cette pa
 
 
 <?php
-require_once($fichiersInclude.'footer.php'); 
+require_once($fichiersInclude.'footer.html'); 
 ?>
